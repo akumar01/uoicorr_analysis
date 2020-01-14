0832c8d7a36b3abc2d676b17cc6ff192061f733e
akumar01 <akumar.4db0@gmail.com>
2020-01-14 10:02:38 -0800

    syncing info theory work from home computer

--- info_theory/numerics.py
+++ info_theory/numerics.py
@@ -4,9 +4,9 @@ import scipy
 import sys
 import schwimmbad
 import pickle
+import pdb
 
 from dchisq import DChiSq
-
 
 # n : number of samples
 # sigma: noise standard deviation
@@ -19,9 +19,6 @@ def mcdarmiad_bound(epsilon, n, sigma, T, gamma, Delta):
 
     c1 = sigma**2 * (T + 2 * epsilon_1 + 2 * np.sqrt(T * epsilon_1))
     c2 = gamma**2 * ((n - T) + 2 * epsilon_2 + 2 * np.sqrt((n - T) * epsilon_2))
-
-    # Measure with respect to the mean for consistency with the chernoff bound
-    Delta0 = 
 
     prob = np.exp(-epsilon_1) + np.exp(-epsilon_2) + \
             np.exp(-2 * (Delta - (np.exp(-epsilon_1 + np.exp(-epsilon_2))) * (c1 + c2))**2/\
@@ -37,33 +34,35 @@ def mcdarmiad_bound(epsilon, n, sigma, T, gamma, Delta):
 # t: free parameter in Chernoff bounding technique
 def chernoff_bound(t, n, sigma, T, gamma, Delta):
 
+    t = t[0]
+
     # Optimize the log
     log_prob = -T/2 * np.log(1 - 2 * sigma**2 * t) - (n - T)/2 * np.log(1 + 2 * gamma**2 * t) - \
     t * (sigma**2 * T - gamma*2 * (n - T)) - t * Delta
-
     return log_prob
 
 # Use the Gil-Palaez inversion formuale
 def direct_bound(n, sigma, T, gamma, Delta):
+    # Note the ordering here is different than the large deviation bounds due 
+    # to the sign of the inequality
 
-    dchi2 = DChiSq(sigma**2, gamma**2, T, n - T)
+    dchi2 = DChiSq(gamma**2, sigma**2, n - T, T)
     error_prob = dchi2.nCDF(Delta)
-
     return error_prob
 
 
-def calc_error_probabilities(n, sigma, T, gamma, Delta):
+def calc_error_probabilities(n, sigma, T, gamma, Delta, Delta0):
 
     # Optimize Chernoff bound - start at 0 and bound to the range of the MGF
-    bounds = (0, 1/(2 * sigma**2))
+    bounds = [(0, 1/(2 * sigma**2))]
 
-    optimal_chernoff_bound = scipy.optimize(chernoff_bound, 0, args=(n, sigma, T, gamma, Delta), bounds=bounds)
+    optimal_chernoff_bound = scipy.optimize.minimize(chernoff_bound, 0, args=(n, sigma, T, gamma, Delta), bounds=bounds).fun
 
     # Optimize the McDarmiad Bound
-    optimal_mcdarmiad_bound = scipy.optimize(mcdarmiad_bound, [1, 1], 
-                              args = (n, sigma, T, gamma, Delta))
+    optimal_mcdarmiad_bound = scipy.optimize.minimize(mcdarmiad_bound, [1, 1], 
+                              args = (n, sigma, T, gamma, Delta)).fun
 
-    actual_prob = direct_bound(n, sigma, T, gamma, Delta)
+    actual_prob = direct_bound(n, sigma, T, gamma, Delta0)
 
     return actual_prob, optimal_chernoff_bound, optimal_mcdarmiad_bound
 
@@ -94,17 +93,17 @@ def nested_model_selection(task_tuple):
                 # Note that the sign is reversed because we have reversed the order of
                 # the difference to take an upper tail bound
                 Delta = sigma**2 * penalty * (T - S)        
-
+                # normal ordering that we put into the direct bound
+                Delta0 = sigma**2 * penalty * (S - T)
                 # Need to measure deviation from the mean
                 mu = sigma**2 * T_ - gamma**2 * (n - T_)
 
-                e1, e2, e3 = calc_error_probabilities(n, sigma, T_, gamma, Delta - mu) 
+                e1, e2, e3 = calc_error_probabilities(n, sigma, T_, gamma, Delta - mu, Delta0) 
                 actual_prob[i, j, k] = e1
                 chernoff_prob[i, j, k] = e2
                 mcdarmiad_prob[i, j, k] = e3
-
-    return (actual prob, chernoff_prob, mcdarmiad_prob)
-
+    probs = (actual_prob, chernoff_prob, mcdarmiad_prob) 
+    return probs
 # Sweep over problem parameters via schwimmbad
 # All arguments should be ndarray-like
 def sweep_problem_params(sigma, gamma, p, savename):
