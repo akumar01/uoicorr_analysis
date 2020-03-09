5e63d792a8dd6b9eb407439b83072bac6ec2ad80
akumar01 <akumar.4db0@gmail.com>
2020-03-08 22:46:56 -0700

    syncing notebooks

--- sigmoid.py
+++ sigmoid.py
@@ -2,6 +2,7 @@ import pdb
 import numpy as np 
 import scipy as sp
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 
 # Fit a sigmoid to X and y
 
@@ -9,8 +10,8 @@ from sklearn.linear_model import LinearRegression
 
 class Sigmoid():
 
-    def __init__(self):
-        pass
+    def __init__(self, robust=False):
+        self.robust = robust        
 
     def sigmoid_fn(self, z, L, k, z0, c):
 
@@ -28,10 +29,125 @@ class Sigmoid():
         # Bound parameters within data range
         bounds = ([0, 0, min(X), 0], [1, max(X) - min(X), max(X), 1])
 
-        # Test without jacobian first, add if ncesssary
+        if self.robust:
 
-        opt_result = sp.optimize.curve_fit(self.sigmoid_fn, X, y, p0=[L0, k0, z0_0, c0],
-                              bounds=bounds)
+            sigmoid_loss = lambda params, xx, yy: self.sigmoid_fn(xx, *params) - yy
 
-        self.coef_ = opt_result[0]
-        self.coef_cov = opt_result[1]
+            # Use Huber loss
+            opt_result = sp.optimize.least_squares(sigmoid_loss, x0=[L0, k0, z0_0, c0], args=(X, y),
+                                                   bounds=bounds, loss='huber')
+
+            self.coef_ = opt_result.x
+
+        else:
+
+            # Ordinary least squares
+            opt_result = sp.optimize.curve_fit(self.sigmoid_fn, X, y, p0=[L0, k0, z0_0, c0],
+                                 bounds=bounds)
+
+            self.coef_ = opt_result[0]
+            self.coef_cov = opt_result[1]
+
+
+    def predict(self, X):
+
+        return self.sigmoid_fn(X, *self.coef_)
+
+class Exponen():
+
+    def __init__(self, robust=False):
+        self.robust = robust
+
+    def exp_fn(self, z, L, k, z0, c):
+
+        return c + L * np.exp(-k * (z - z0))
+
+    def fit(self, X, y):
+
+        # Set initial parameters using some basic heuristics
+        c0 = min(y)
+        L0 = 0.25
+        z0_0 = np.min(X) + (np.max(X) - np.min(X))/2
+        k0 = np.abs(LinearRegression(normalize=True, fit_intercept=True).fit(X.reshape(-1, 1), y).coef_.ravel()[0])
+
+        # Bound paramters within data range
+        bounds = ([0, 0, min(X), 0], [1, max(X) - min(X), max(X), 1])
+
+        if self.robust:
+
+            expon_loss = lambda params, xx, yy: self.exp_fn(xx, *params) - yy
+
+            # Use Huber loss
+            try:
+                opt_result = sp.optimize.least_squares(expon_loss, x0=[L0, k0, z0_0, c0], args=(X, y),
+                                                       bounds=bounds, loss='huber')
+            except:
+                pdb.set_trace()
+            
+            self.coef_ = opt_result.x
+
+        else:
+
+            # Ordinary least squares
+            opt_result = sp.optimize.curve_fit(self.exp_fn, X, y, p0=[L0, k0, z0_0, c0],
+                                 bounds=bounds)
+
+            self.coef_ = opt_result[0]
+            self.coef_cov = opt_result[1]
+
+    def predict(self, X):
+
+        return self.exp_fn(X, *self.coef_)
+
+
+class AverageModel():
+
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+
+        # Set the offset to the average value
+        self.coef_ = [np.nan, np.nan, np.nan, np.mean(y)]
+
+    def predict(self, X):
+
+        return self.coef_[-1] * np.ones(X.size)
+
+
+
+# Class to try and fit all 3 models to the data and choose the one that does the best w.r.t to R^2
+class Fit_and_Select():
+
+    def __init__(self):
+
+        pass
+
+    def fit(self, X, y):
+
+        # Try all three models
+        avg_model = AverageModel()
+        avg_model.fit(X, y)
+        y_pred0 = avg_model.predict(X)
+
+        expon_model = Exponen(robust=True)
+        expon_model.fit(X, y)
+        y_pred1 = expon_model.predict(X)
+
+        sigmoid_model = Sigmoid(robust=True)
+        sigmoid_model.fit(X, y)
+        y_pred2 = sigmoid_model.predict(X)
+
+        # Score models
+        r2_avg = r2_score(y, y_pred0)
+
+        r2_expon = r2_score(y, y_pred1)
+
+        r2_sigmoid = r2_score(y, y_pred2)
+
+        if max([r2_avg, r2_expon, r2_sigmoid]) == r2_avg:
+            self.coef_ = avg_model.coef_
+        elif max([r2_avg, r2_expon, r2_sigmoid]) == r2_expon:
+            self.coef_ = expon_model.coef_
+        elif max([r2_avg, r2_expon, r2_sigmoid]) == r2_sigmoid:
+            self.coef_ = sigmoid_model.coef_
